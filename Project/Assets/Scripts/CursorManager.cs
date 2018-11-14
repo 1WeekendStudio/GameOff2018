8a097164407e276faf66e3644121c49540390f0f
Adrien Allard <adrien.allard.pro@gmail.com>
2018-11-14 08:12:45 +0100

    Add cursor manager

---
+++ Project/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using View;
+
+public class CursorManager : MonoBehaviour
+{
+    private Dictionary<System.Type, ICursor> cursors = new Dictionary<Type, ICursor>();
+    private ICursor currentCursor;
+
+    private Camera currentCamera = null;
+
+    public static CursorManager Instance { get; private set; }
+
+    public void ChangeCursor<T>(object parameter) where T : ICursor
+    {
+        this.currentCursor?.OnDeactivate();
+
+        if (!this.cursors.TryGetValue(typeof(T), out this.currentCursor))
+        {
+            Debug.LogError($"Unknown cursor: {typeof(T)}");
+            return;
+        }
+
+        this.currentCursor.OnActivate(parameter);
+    }
+
+    private void Awake()
+    {
+        Debug.Assert(CursorManager.Instance == null);
+        CursorManager.Instance = this;
+
+        this.cursors.Add(typeof(PlantPlacementCursor), new PlantPlacementCursor());
+    }
+
+    private void Update()
+    {
+        if (this.currentCamera == null)
+        {
+            this.currentCamera = GameObject.FindObjectOfType<Camera>();
+            return;
+        }
+
+        this.currentCursor?.Update(this.currentCamera);
+    }
+
+    private void OnDrawGizmos()
+    {
+        this.currentCursor?.OnDrawGizmos();
+    }
+}
---
+++ Project/Assets/Scripts/ICursor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public interface ICursor
+{
+    void OnActivate(object parameter);
+
+    void Update(Camera camera);
+
+    void OnDeactivate();
+
+    void OnDrawGizmos();
+}
