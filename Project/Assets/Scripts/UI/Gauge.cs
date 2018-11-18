d47b1efbee6509f32643c2e080c7c38963a55fce
Adrien Allard <adrien.allard.pro@gmail.com>
2018-11-18 11:03:15 +0100

    Add gauge for water and sun properties

---
+++ Project/Assets/Scripts/UI/Gauge.cs
@@ -0,0 +1,57 @@
+﻿namespace UI
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Gauge : MonoBehaviour
+    {
+        private const float MaxValue = 10f;
+
+        [SerializeField]
+        private Image value;
+
+        [SerializeField]
+        private Image min;
+
+        [SerializeField]
+        private Image max;
+
+        private RectTransform rectTransform;
+
+        public int Value
+        {
+            set
+            {
+                float position = (value / MaxValue) * this.rectTransform.rect.width;
+                this.value.rectTransform.anchoredPosition = new Vector2(position, 0);
+            }
+        }
+
+        public int Min
+        {
+            set
+            {
+                float position = (value / MaxValue) * this.rectTransform.rect.width;
+                this.min.rectTransform.anchoredPosition = new Vector2(position, 0);
+            }
+        }
+
+        public int Max
+        {
+            set
+            {
+                float position = (value / MaxValue) * this.rectTransform.rect.width;
+                this.max.rectTransform.anchoredPosition = new Vector2(position, 0);
+            }
+        }
+
+        private void Start()
+        {
+            this.rectTransform = this.GetComponent<RectTransform>();
+        }
+        
+        private void Update()
+        {
+        }
+    }
+}
--- Project/Assets/Scripts/UI/PlantTooltip.cs
+++ Project/Assets/Scripts/UI/PlantTooltip.cs
@@ -1,53 +1,63 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿namespace UI
+{ 
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class PlantTooltip : MonoBehaviour
-{
-    [SerializeField]
-    private RectTransform panel;
-
-    [SerializeField]
-    private Text title;
-
-    [SerializeField]
-    private Text waterProperty;
-
-    [SerializeField]
-    private Text sunProperty;
-
-    [SerializeField]
-    private Text windProperty;
-
-    [SerializeField]
-    private Text elevationProperty;
-
-    private void Start()
+    public class PlantTooltip : MonoBehaviour
     {
-    }
-    
-    private void Update()
-    {
-        if (CursorManager.Instance == null || CursorManager.Instance.HoveredPlot == null)
+        [SerializeField]
+        private RectTransform panel;
+
+        [SerializeField]
+        private Text title;
+
+        [SerializeField]
+        private Gauge waterProperty;
+
+        [SerializeField]
+        private Gauge sunProperty;
+
+        [SerializeField]
+        private Text windProperty;
+
+        [SerializeField]
+        private Text elevationProperty;
+
+        private void Start()
         {
-            this.panel.gameObject.SetActive(false);
-            return;
         }
-
-        Position position = CursorManager.Instance.HoveredPlotPosition;
-
-        SoilTile soilTile = CursorManager.Instance.HoveredPlot.Soil[position.X, position.Y];
-
-        if (soilTile.Plant == null)
+        
+        private void Update()
         {
-            this.panel.gameObject.SetActive(false);
-            return;
-        }
+            if (CursorManager.Instance == null || CursorManager.Instance.HoveredPlot == null)
+            {
+                this.panel.gameObject.SetActive(false);
+                return;
+            }
 
-        this.panel.gameObject.SetActive(true);
-        this.title.text = position.ToString();
-        this.waterProperty.text = $"{soilTile.WaterLevel} / {soilTile.Plant.Description.MinimumWater} - {soilTile.Plant.Description.MaximumWater}";
-        this.sunProperty.text = $"{soilTile.SunshineLevel} / {soilTile.Plant.Description.MinimumSunshine} - {soilTile.Plant.Description.MaximumSunshine}";
-        this.windProperty.text = $"{soilTile.WindLevel} / {soilTile.Plant.Description.WindResistance}";
-        this.elevationProperty.text = soilTile.Elevation.ToString();
+            Position position = CursorManager.Instance.HoveredPlotPosition;
+
+            SoilTile soilTile = CursorManager.Instance.HoveredPlot.Soil[position.X, position.Y];
+
+            if (soilTile.Plant == null)
+            {
+                this.panel.gameObject.SetActive(false);
+                return;
+            }
+
+            this.panel.gameObject.SetActive(true);
+            this.title.text = position.ToString();
+
+            this.waterProperty.Value = soilTile.WaterLevel;
+            this.waterProperty.Min = soilTile.Plant.Description.MinimumWater;
+            this.waterProperty.Max = soilTile.Plant.Description.MaximumWater;
+
+            this.sunProperty.Value = soilTile.SunshineLevel;
+            this.sunProperty.Min = soilTile.Plant.Description.MinimumSunshine;
+            this.sunProperty.Max = soilTile.Plant.Description.MaximumSunshine;
+
+            this.windProperty.text = $"{soilTile.WindLevel} / {soilTile.Plant.Description.WindResistance}";
+            this.elevationProperty.text = soilTile.Elevation.ToString();
+        }
     }
 }
