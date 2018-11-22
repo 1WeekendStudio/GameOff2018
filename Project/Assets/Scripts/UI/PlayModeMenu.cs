bdf99b492f41b59c4da4e0c51f5cba6fe4c89c88
Adrien Allard <adrien.allard.pro@gmail.com>
2018-11-22 08:06:17 +0100

    Add playmode UI

---
+++ Project/Assets/Scripts/UI/PlayModeMenu.cs
@@ -0,0 +1,29 @@
+﻿namespace UI
+{
+    using Data;
+
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    using View;
+
+    public class PlayModeMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private Text changeModeButtonText;
+        
+        public void ChangeModeCallback()
+        {
+            if (CursorManager.Instance.CurrentCursor is GardeningCursor)
+            {
+                CursorManager.Instance.ChangeCursor<DefaultCursor>();
+                this.changeModeButtonText.text = "Do some gardening";
+            }
+            else if (CursorManager.Instance.CurrentCursor is DefaultCursor)
+            {
+                CursorManager.Instance.ChangeCursor<GardeningCursor>();
+                this.changeModeButtonText.text = "Enjoy your garden";
+            }
+        }
+    }
+}
